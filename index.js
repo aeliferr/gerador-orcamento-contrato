35144d747c5a4b56ee005c371e92c4f4a7fe3013
Abner Ferrari <aeliferrari@gmail.com>
2024-09-01 23:20:02 -0300

    imprimindo pdf

--- index.js
+++ index.js
@@ -1,5 +1,60 @@
-import express from "express";
+const PdfPrinter = require('pdfmake');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
+app.use(bodyParser.json());
 
-app.listen(4000)
+app.post('/generate-pdf', async (req, res) => {
+    try {
+        // Define font files
+        var fonts = {
+            Roboto: {
+                normal: 'fonts/Roboto-Regular.ttf',
+                bold: 'fonts/Roboto-Medium.ttf',
+                italics: 'fonts/Roboto-Italic.ttf',
+                bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+            }
+        };
+
+        var printer = new PdfPrinter(fonts);
+
+        var dd = {
+            content: [
+                'First paragraph',
+                'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
+            ]
+            
+        }
+
+        var options = {
+            // ...
+        }
+
+        var chunks = [];
+
+        var pdfDoc = printer.createPdfKitDocument(dd, options);
+        
+        pdfDoc.on('data', (chunk) => {
+            chunks.push(chunk)
+        })
+
+        pdfDoc.on('end', () => {
+            const result = Buffer.concat(chunks)
+            res.setHeader('Content-Disposition', 'attachment; filename="invoice.pdf"');
+            res.setHeader('Content-Type', 'application/pdf');
+            res.contentType('application/pdf')
+            res.send(result)
+        })
+
+        pdfDoc.end()
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while generating the PDF.');
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
