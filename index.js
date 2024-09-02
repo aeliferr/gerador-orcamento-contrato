739799a5e455b4e8b6b9b6cec8728e84ab82b534
Abner Ferrari <aeliferrari@gmail.com>
2024-09-02 17:29:01 -0300

    generate budget according to input

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const PdfPrinter = require('pdfmake');
 const express = require('express');
+var cors = require('cors')
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 app.post('/generate-pdf', async (req, res) => {
     try {
+        const {vendorName, clientName, items } = req.body
         // Define font files
         var fonts = {
             Roboto: {
@@ -19,12 +22,67 @@ app.post('/generate-pdf', async (req, res) => {
 
         var printer = new PdfPrinter(fonts);
 
+
+        let id = 0
+        const tableItems = items.map((item) => {
+            id++
+            return [id, item.description, new Intl.NumberFormat('pt-Br', { style: 'currency', currency: 'BRL'}).format(item.unitValue), item.quantity]
+        })
         var dd = {
+            pageMargins: [20, 30, 20, 20], // [left, top, right, bottom]
             content: [
-                'First paragraph',
-                'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
-            ]
-            
+                {
+                    image: 'logo_artfaav_rgb.png',
+                    width: 150,
+                    style: 'center'
+                },
+                {
+                    text: `Cliente: ${clientName}`,
+                    style: 'subheader',
+                    alignment: 'left'
+                },
+                {
+                    text: `Data de validade: ${new Date().toLocaleDateString()}`,
+                    style: 'subheader',
+                    alignment: 'left'
+                },
+                {
+                    text: `Vendedor: ${vendorName}`,
+                    style: 'subheader',
+                    alignment: 'left'
+                },
+                {
+                    margin: [0, 20, 0, 0],
+                    table: {
+                        headerRows: 1,
+                        widths: [20, 350, '*', 30],
+                        body: [
+                            [
+                                { text: 'ID', bold: true },
+                                { text: 'Descrição', bold: true },
+                                { text: 'Valor unitário', bold: true },
+                                { text: 'Qtd.', bold: true }
+                            ],
+                            ...tableItems
+                            // Add more rows as needed
+                        ]
+                    },
+                    layout: 'lightHorizontalLines' // Optional layout style
+                }
+            ],
+            styles: {
+                center: {
+                    alignment: 'center'
+                },
+                left: {
+                    alignment: 'left'
+                },
+                subheader: {
+                    fontSize: 12,
+                    margin: [0, 10, 0, 0] // Top, right, bottom, left margins
+                }
+            }
+
         }
 
         var options = {
@@ -34,7 +92,7 @@ app.post('/generate-pdf', async (req, res) => {
         var chunks = [];
 
         var pdfDoc = printer.createPdfKitDocument(dd, options);
-        
+
         pdfDoc.on('data', (chunk) => {
             chunks.push(chunk)
         })
